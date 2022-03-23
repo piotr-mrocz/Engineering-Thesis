a7d3e6b035e8ffb39a96d5b97bdfca77b89ac15c
piotr-mrocz <p.mrocz@vp.pl>
2022-03-23 20:57:35 +0100

    Clean code + add GenericRepository as Singleton

--- Api/IntranetWebApi/IntranetWebApi/Program.cs
+++ Api/IntranetWebApi/IntranetWebApi/Program.cs
@@ -1,29 +1,36 @@
 using IntranetWebApi.Data;
+using IntranetWebApi.Repository;
 using IntranetWebApi.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);  
+var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+#region Add services to the container
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+#endregion Add services to the container
 
-// database
+#region Database
 builder.Services.AddDbContext<IntranetDbContext>(x =>
     x.UseSqlServer(builder.Configuration.GetConnectionString(nameof(ConnectionString.JDPIntranet))));
+#endregion Database
+
 var app = builder.Build();
 
-// swagger
+#region Swagger
 app.UseSwaggerUI();
 app.UseSwagger(x => x.SerializeAsV2 = true);
+#endregion Swagger
 
-// Configure the HTTP request pipeline.
+#region Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwagger();
 }
+#endregion Configure the HTTP request pipeline.
 
 // register endpoints
 app.RegisterEndpoints();
