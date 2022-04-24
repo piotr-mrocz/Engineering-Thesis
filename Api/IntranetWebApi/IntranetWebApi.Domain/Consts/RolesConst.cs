7336ac755d207d2d9453fc495b5529c6e5d5681e
piotr-mrocz <p.mrocz@vp.pl>
2022-04-24 17:07:32 +0200

    Create const with roles to use in authorize attributes to not hardcoded values

---
+++ Api/IntranetWebApi/IntranetWebApi.Domain/Consts/RolesConst.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntranetWebApi.Domain.Consts;
+
+public static class RolesConst
+{
+    public const string Admin = "Admin";
+    public const string User = "User";
+    public const string Manager = "Manager";
+}
