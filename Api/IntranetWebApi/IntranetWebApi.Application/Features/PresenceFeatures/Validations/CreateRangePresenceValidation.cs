4b4b298be3a8cab51203992654d041820a1b5bc2
piotr-mrocz <p.mrocz@vp.pl>
2022-04-29 20:19:15 +0200

    Create new presence features

---
+++ Api/IntranetWebApi/IntranetWebApi.Application/Features/PresenceFeatures/Validations/CreateRangePresenceValidation.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using IntranetWebApi.Application.Features.PresenceFeatures.Commands;
+using IntranetWebApi.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntranetWebApi.Application.Features.PresenceFeatures.Validations;
+
+public class CreateRangePresenceValidation : AbstractValidator<CreateRangePresencesCommand>
+{
+    public CreateRangePresenceValidation(IntranetDbContext dbContext)
+    {
+        RuleFor(x => x.ListOfPresences)
+            .NotEmpty()
+            .WithMessage("Nie podano żadnych danych!");
+
+        RuleForEach(x => x.ListOfPresences.Select(x => x.Date))
+            .NotNull()
+            .NotEmpty()
+            .WithMessage("");
+
+        RuleForEach(x => x.ListOfPresences).SetValidator(new CreatePresenceValidation());
+    }
+}
