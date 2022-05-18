0be8702efed5a49a90e33cd744dabbb599bd754a
piotr-mrocz <p.mrocz@vp.pl>
2022-05-18 19:31:08 +0200

    Add login form

--- Client/src/app/components/login/login.component.ts
+++ Client/src/app/components/login/login.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Types } from 'src/app/models/enums/types.enum';
 import { LoginDto } from 'src/app/models/dto/loginDto';
+import { FormGroup } from '@angular/forms';
+import { AuthenticationService } from 'src/app/services/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,12 +12,16 @@ import { LoginDto } from 'src/app/models/dto/loginDto';
 })
 export class LoginComponent {
 
+  showLoginErrorMessage: boolean = false;
+  showPasswordErrorMessage: boolean = false;
+  errorMessage: string;
+
   hiddenPassword: boolean = true;
   passwordType: string = Types[Types.password];
 
 model: LoginDto;
 
-  constructor() { }
+  constructor(private authService: AuthenticationService, private router: Router) { }
 
   showPassword() {
     this.hiddenPassword = false;
@@ -26,7 +33,52 @@ model: LoginDto;
     this.passwordType = Types[Types.password];
   }
 
-  logIn() {
-      console.log(this.model);
+  logIn(login: string, password: string) {
+    if (this.validateInputs(login, password)) {
+      console.log(login);
+      console.log(password);
+    }
+  }
+
+  validateInputs(login: string, password: string) {
+    if (login.trim().length == 0 || password.trim().length == 0) {
+      this.showLoginErrorMessage = true;
+      this.showPasswordErrorMessage = true;
+      this.errorMessage = "Pole wymagane!";
+
+      return false;
+    }
+
+    if (login.trim().length == 0) {
+      this.showLoginErrorMessage = true;
+      this.errorMessage = "Pole wymagane!";
+
+      return false;
+    }
+
+    if (password.trim().length == 0) {
+      this.showPasswordErrorMessage = true;
+      this.errorMessage = "Pole wymagane!";
+
+      return false;
+    }
+
+    this.showLoginErrorMessage = false;
+    this.showPasswordErrorMessage = false;
+    this.errorMessage = "";
+
+    return true;
+  }
+
+  resetLoginError(event: KeyboardEvent) {
+    if (event.keyCode != 32) { // space
+      this.showLoginErrorMessage = false;
+    }
+  }
+
+  resetPasswordError(event: KeyboardEvent) {
+    if (event.keyCode != 32) { // space
+      this.showPasswordErrorMessage = false;
+    }
   }
 }
