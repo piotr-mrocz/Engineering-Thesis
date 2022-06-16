d4ae521fc36a7aae08b5ba8de788d4a374060564
piotr-mrocz <p.mrocz@vp.pl>
2022-06-16 19:04:00 +0200

    Add two columns to users table

---
+++ Api/IntranetWebApi/IntranetWebApi.Infrastructure/Migrations/20220616170129_AddTwoColumnsToUsersTable.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IntranetWebApi.Infrastructure.Migrations
+{
+    public partial class AddTwoColumnsToUsersTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Users");
+        }
+    }
+}
