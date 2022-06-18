c9fab53599cc5c571f54428ebc942c06cc1970b7
piotr-mrocz <p.mrocz@vp.pl>
2022-06-18 16:12:19 +0200

    Go to login page after token has expired

--- Client/src/app/services/token-interceptor.service.ts
+++ Client/src/app/services/token-interceptor.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Injector } from '@angular/core';
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from './authentication.service';
+import { Router } from '@angular/router';
 
 export const delayMs = 2000;
 
@@ -10,15 +11,22 @@ export const delayMs = 2000;
 })
 export class TokenInterceptorService implements HttpInterceptor{
 
-  constructor(private inject: Injector) { }
+  constructor(private inject: Injector, private router: Router) { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let authService = this.inject.get(AuthenticationService);
  
     var token = authService.getToken();
-    req = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${token}`)
-    });
 
+    if (authService.isTokenExpired(token)) {
+      console.log("Token expired");
+      this.router.navigate(['/login']);
+    }
+    else {
+        req = req.clone({
+        headers: req.headers.set('Authorization', `Bearer ${token}`)
+      });
+    }
+    
     return next.handle(req);
   }
 }
