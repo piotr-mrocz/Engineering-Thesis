b5dd36c080edb975f2b9c7974b1cf15441315c0f
piotr-mrocz <p.mrocz@vp.pl>
2022-06-19 12:10:50 +0200

    Cosmetic changes

--- Api/IntranetWebApi/IntranetWebApi/Middleware/ErrorHandlerMiddleware.cs
+++ Api/IntranetWebApi/IntranetWebApi/Middleware/ErrorHandlerMiddleware.cs
@@ -24,7 +24,12 @@ namespace IntranetWebApi.Middleware
             {
                 var response = context.Response;
                 response.ContentType = "application/json";
-                var responseModel = new Response<string>() { Succeeded = false, Message = error?.Message };
+
+                var responseModel = new Response<string>() 
+                { 
+                    Succeeded = false, 
+                    Message = error?.Message 
+                };
 
                 switch (error)
                 {
