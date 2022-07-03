47e0e7c5c5c0c563bcee5e3b4a451f812a7d19f1
piotr-mrocz <p.mrocz@vp.pl>
2022-07-03 16:58:45 +0200

    add enum for task's status

---
+++ Api/IntranetWebApi/IntranetWebApi.Domain/Enums/TaskStatusEnum.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace IntranetWebApi.Domain.Enums;
+
+public enum TaskStatusEnum
+{
+    [Description("Do zrobienia")]
+    ToDo = 1,
+
+    [Description("W trakcie wykonywania")]
+    InProgress = 2,
+
+    [Description("Gotowe")]
+    Done = 3
+}
