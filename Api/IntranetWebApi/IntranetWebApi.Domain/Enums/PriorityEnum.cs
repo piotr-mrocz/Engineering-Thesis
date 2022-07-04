6302da0134d818e6b432633bb26820e7f5b52fd1
piotr-mrocz <p.mrocz@vp.pl>
2022-07-04 19:21:00 +0200

    Create enum for priority task

---
+++ Api/IntranetWebApi/IntranetWebApi.Domain/Enums/PriorityEnum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntranetWebApi.Domain.Enums;
+
+public enum PriorityEnum
+{
+    [Description("Bardzo pilne")]
+    VeryUrgent = 1,
+
+    [Description("Pilne")]
+    Urgent = 2,
+
+    [Description("Ważne")]
+    Important = 3,
+
+    [Description("Może poczekać")]
+    CanWait = 4
+}
