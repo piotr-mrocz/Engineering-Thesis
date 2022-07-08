7c7e7fcc663d0d356e30da6212ac07edf2fd988b
piotr-mrocz <p.mrocz@vp.pl>
2022-07-08 17:48:40 +0200

    Add migration. Change column type and add new column

---
+++ Api/IntranetWebApi/IntranetWebApi.Infrastructure/Migrations/20220708154721_ChangeColumnType.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IntranetWebApi.Infrastructure.Migrations
+{
+    public partial class ChangeColumnType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsAcceptedBySupervisor",
+                table: "RequestForLeaves",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DaysAbsence",
+                table: "RequestForLeaves",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DaysAbsence",
+                table: "RequestForLeaves");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsAcceptedBySupervisor",
+                table: "RequestForLeaves",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+    }
+}
