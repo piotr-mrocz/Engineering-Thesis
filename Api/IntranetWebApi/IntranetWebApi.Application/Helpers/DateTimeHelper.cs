15dfea4405a9145758174e59326e2fc341bbb71e
piotr-mrocz <p.mrocz@vp.pl>
2022-07-09 02:59:33 +0200

    Add helper to calculate total days between 2 dates without weekends

---
+++ Api/IntranetWebApi/IntranetWebApi.Application/Helpers/DateTimeHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntranetWebApi.Application.Helpers;
+
+public static class DateTimeHelper
+{
+    public static int CalculateTotalDaysBetweenDatesWithoutWeekends(DateTime startDate, DateTime endDate)
+    {
+        var totalDaysVacation = (int)(endDate.Date - startDate.Date).TotalDays + 1;
+
+        for (DateTime i = startDate.Date; i <= endDate.Date; i.Date.AddDays(1))
+        {
+            if (i.DayOfWeek == DayOfWeek.Saturday || i.DayOfWeek == DayOfWeek.Sunday)
+            {
+                totalDaysVacation = totalDaysVacation--;
+            }
+        }
+
+        return totalDaysVacation;
+    }
+}
