89774138611ee1f703e9acc503b823cf482348d2
piotr-mrocz <p.mrocz@vp.pl>
2022-07-16 00:29:57 +0200

    Function to get all system message depending on idUser - init

---
+++ Api/IntranetWebApi/IntranetWebApi.Application/Features/SystemMessagesFeatures/Queries/GetAllUnReadSystemMessageQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IntranetWebApi.Domain.Models.Entities;
+using IntranetWebApi.Models.Response;
+using MediatR;
+
+namespace IntranetWebApi.Application.Features.SystemMessagesFeatures.Queries;
+
+public class GetAllUnReadSystemMessageQuery : IRequest<Response<SystemMessageListDto>>
+{
+}
+
+public class SystemMessageListDto
+{
+    public List<SystemMessage> SystemMessages { get; set; } = new();
+}
---
+++ Api/IntranetWebApi/IntranetWebApi/Controllers/SystemMessageController.cs
@@ -0,0 +1,21 @@
+﻿using IntranetWebApi.Application.Features.SystemMessagesFeatures.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IntranetWebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class SystemMessageController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public SystemMessageController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> GetAllUnReadSystemMessage(GetAllUnReadSystemMessageQuery request)
+        => Ok(await _mediator.Send(request));
+}
